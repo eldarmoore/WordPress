52bffc0f7131b30b216e4fdda9be0d31b81a7521
Eldar <eldar.moore@gmail.com>
2020-02-17 03:37:15 +0200

    Changes: 1. Form Validation (Frontend - if empty)

--- themes/premierproject/functions.php
+++ themes/premierproject/functions.php
@@ -4,6 +4,23 @@ get_template_part('includes/load', 'assets');
 
 get_template_part('includes/send', 'email');
 
+function ValidatePhone($phone){
+    if(preg_match('/^\+[0-9]{1,2}-[0-9]{3}-[0-9]{3}-[0-9]{4}$/', $phone)) {
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function ValidateName($name)
+{
+    if(!preg_match('/^[a-z]*$/i', $name)){
+        return true;
+    } else {
+        return false;
+    }
+}
+
 // If Logged in
 add_action('wp_ajax_enquiry', 'enquiry_form');
 
@@ -16,6 +33,11 @@ function enquiry_form()
     $formdata = [];
 
     wp_parse_str($_POST['enquiry'], $formdata);
+
+    // sanitize form values
+    $formdata['name'] = ValidateName($formdata['name']);
+    $formdata['email'] = sanitize_email($formdata['email']);
+    $formdata['name'] = ValidatePhone($formdata['phone']);
 
     // Send Email
     SendEmail($formdata);
--- themes/premierproject/includes/form-enquiry.php
+++ themes/premierproject/includes/form-enquiry.php
@@ -6,12 +6,15 @@
             <div class="col-md-6">
                 <div class="form-group">
                     <input type="text" name="name" class="form-control" placeholder="Your Name *" value="" />
+                    <span id="name_error"></span>
                 </div>
                 <div class="form-group">
                     <input type="email" name="email" class="form-control" placeholder="Your Email *" value="" />
+                    <span id="email_error"></span>
                 </div>
                 <div class="form-group">
-                    <input type="number" name="phone" class="form-control" placeholder="Your Phone Number *" value="" />
+                    <input type="text" name="phone" class="form-control" placeholder="Your Phone Number *" value="" />
+                    <span id="phone_error"></span>
                 </div>
                 <div class="form-group">
                     <input type="submit" name="btnSubmit" class="btnContact" value="Send Message" />
@@ -31,37 +34,64 @@
 <script>
 
     (function($) {
+
+        /**
+         * @return {boolean}
+         */
+        function IfInputEmpty($element) {
+            if($element.val() === ''){
+                $($element).addClass("is-invalid");
+                $($element).nextAll('span:first').text("You need to fill this row");
+                return false;
+            } else {
+                $($element).removeClass("is-invalid");
+                $($element).nextAll('span:first').text("");
+                return true;
+            }
+        }
+
         $('#enquiry').submit(function (event) {
 
             event.preventDefault();
 
-            var endpoint = '<?php echo admin_url('admin-ajax.php');?>';
+            // Validate each input if empty
+            let isNameEmpty = IfInputEmpty($("input[name=name"));
+            let isEmailEmpty = IfInputEmpty($("input[name=email"));
+            let isPhoneEmpty = IfInputEmpty($("input[name=phone"));
 
-            var form = $('#enquiry').serialize();
+            if (isNameEmpty === true && isEmailEmpty === true && isPhoneEmpty === true) {
 
-            var formdata = new FormData;
+                $(this).removeClass("is-invalid");
 
-            // Send Action
-            formdata.append('action', 'enquiry');
+                var endpoint = '<?php echo admin_url('admin-ajax.php');?>';
 
-            // Send Data
-            formdata.append('enquiry', form);
+                var form = $('#enquiry').serialize();
 
-            $.ajax(endpoint, {
-                type: 'POST',
-                data: formdata,
-                processData: false,
-                contentType: false,
+                var formdata = new FormData();
 
-                success: function (res) {
-                    alert(res.data);
-                },
+                // Send Action
+                formdata.append('action', 'enquiry');
 
+                // Send Data
+                formdata.append('enquiry', form);
 
-                error: function (err) {
+                $.ajax(endpoint, {
+                    type: 'POST',
+                    data: formdata,
+                    processData: false,
+                    contentType: false,
 
-                }
-            })
+                    success: function (res) {
+                        alert(res.data);
+                    },
+
+                    error: function (err) {
+
+                    }
+                });
+            } else {
+
+            }
 
         })
 
